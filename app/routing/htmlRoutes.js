2058edcfeb94900348d118ff5f0da970e2958bd9
Melissa Chiang <mchiang62@gmail.com>
2019-11-17 18:27:13 -0500

    htmlRoutes done

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,18 @@
+// path file to get the correct file path for html
+var path = require("path");
+
+module.exports = function (app) {
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+
+    });
+
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+
+
+
+
+
+};
